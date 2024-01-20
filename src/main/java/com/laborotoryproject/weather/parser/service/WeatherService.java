621b88f190218b5b6bb00d333b00ea138b20a74e
alim <asas@gmail.com>
2024-01-20 07:53:51 +0400

    make the List<Weather> method throws exception if weather not found

--- src/main/java/com/laborotoryproject/weather/parser/service/WeatherService.java
+++ src/main/java/com/laborotoryproject/weather/parser/service/WeatherService.java
@@ -2,9 +2,9 @@ package com.laborotoryproject.weather.parser.service;
 
 import com.laborotoryproject.weather.parser.entity.Weather;
 import com.laborotoryproject.weather.parser.exception.request_exceptions.*;
+import com.laborotoryproject.weather.parser.exception.validate_exceptions.StringContainsDigitException;
 import com.laborotoryproject.weather.parser.exception.validate_exceptions.StringNotStartWithDigitException;
 import com.laborotoryproject.weather.parser.repository.WeatherRepository;
-import com.laborotoryproject.weather.parser.util.validate.ValidatingData;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,83 +12,90 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static com.laborotoryproject.weather.parser.util.validate.ValidatingData.*;
+
 @Service
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class WeatherService {
 
     WeatherRepository weatherRepository;
-    ValidatingData validatingData;
 
     @Autowired
     public WeatherService(WeatherRepository weatherRepository) {
         this.weatherRepository = weatherRepository;
-        validatingData = new ValidatingData();
-    }
-
-    public Weather findWeatherByCityName(String citiName) {
-        if (validatingData.checkStringNotContainsDigit(citiName)) {
-            return weatherRepository
-                    .findWeatherByCityName(citiName)
-                    .orElseThrow(() -> new CityNotFoundException("city not found"));
-        } else {
-            throw new StringNotStartWithDigitException("string contains digit");
-        }
     }
 
     public Weather findById(long id) {
         return weatherRepository
                 .findById(id)
-                .orElseThrow(() -> new WeatherWithIdNotFoundException("weather with this id not found"));
+                .orElseThrow(() -> new WeatherNotFound("weather with this id not found"));
     }
 
-    public List<Weather> findByTemperature(String temperature) {
-        if (validatingData.checkTemperatureInput(temperature)) {
+    public Weather findWeatherByCityName(String cityName) {
+        if (checkStringNotContainsDigit(cityName)) {
             return weatherRepository
-                    .findWeatherByTemperature(temperature)
-                    .orElseThrow(() -> new WeatherWithTemperatureNotFoundException("weather with this temperature not found"));
+                    .findWeatherByCityName(cityName)
+                    .orElseThrow(() -> new CityNotFoundException("city not found"));
         } else {
-            throw new StringNotStartWithDigitException("string contains letter");
+            throw new StringContainsDigitException("cityName contains digit");
         }
     }
 
 
-    public List<Weather> findByPressure(String pressure) {
-        if (validatingData.checkStringStartWithDigitAndContainsLetter(pressure)) {
-            return weatherRepository
-                    .findWeatherByPressure(pressure)
-                    .orElseThrow(() -> new WeatherWithPressureNotFoundException("weather with this pressure not found"));
+    public List<Weather> findAllWeathersByTemperature(String temperature) {
+        if (checkTemperatureInput(temperature)) {
+            return weatherRepository.findAllWeathersByTemperature(temperature)
+                    .filter(weathers -> weathers.stream().findFirst().isPresent())
+                    .orElseThrow(() -> new WeatherNotFound("weather with this temperature not found"));
         } else {
-            throw new StringNotStartWithDigitException("string not start with digit or don't contains letter");
+            throw new StringNotStartWithDigitException("temperature contains digit");
         }
     }
 
-    public List<Weather> findBySpeed(String speed) {
-        if (validatingData.checkStringStartWithDigitAndContainsLetter(speed)) {
-            return weatherRepository
-                    .findWeatherBySpeed(speed)
-                    .orElseThrow(() -> new WeatherWithSpeedNotFoundException("weather with this speed not found"));
+
+    public List<Weather> findAllWeathersByPressure(String pressure) {
+        if (checkStringStartWithDigitAndContainsLetter(pressure)) {
+            return weatherRepository.findAllWeathersByPressure(pressure)
+                    .filter(weathers -> weathers.stream().findFirst().isPresent())
+                    .orElseThrow(() -> new WeatherNotFound("weather with this pressure not found"));
         } else {
-            throw new StringNotStartWithDigitException("string not start with digit or don't contains letter");
+            throw new StringNotStartWithDigitException("pressure not start with digit or don't contains 'мм'");
         }
     }
 
-    public List<Weather> findByHumidity(String humidity) {
-        if (validatingData.checkStringStartWithDigitAndContainsLetter(humidity)) {
-            return weatherRepository
-                    .findWeatherByHumidity(humidity)
-                    .orElseThrow(() -> new WeatherWithHumidityNotFoundException("weather with this humidity not found"));
+    public List<Weather> findAllWeathersBySpeed(String speed) {
+        if (checkStringStartWithDigitAndContainsLetter(speed)) {
+            return weatherRepository.findAllWeathersBySpeed(speed)
+                    .filter(weathers -> weathers.stream().findFirst().isPresent())
+                    .orElseThrow(() -> new WeatherNotFound("weather with this speed not found"));
         } else {
-            throw new StringNotStartWithDigitException("string not start with digit or don't contains letter");
+            throw new StringNotStartWithDigitException("speed not start with digit or don't contains 'мc'");
+        }
+    }
+
+
+    public List<Weather> findAllWeathersByHumidity(String humidity) {
+        if (checkStringForHumidity(humidity)) {
+            return weatherRepository.findAllWeathersByHumidity(humidity)
+                    .filter(weathers -> weathers.stream().findFirst().isPresent())
+                    .orElseThrow(() -> new WeatherNotFound("weather with this humidity not found"));
+        } else {
+            throw new StringNotStartWithDigitException("humidity contains digit");
+        }
+    }
+
+
+    public void deleteWeatherById(long id) {
+        if (checkId(String.valueOf(id))) {
+            weatherRepository
+                    .findById(id)
+                    .ifPresent(weatherRepository::delete);
+        } else {
+            throw new StringNotStartWithDigitException("id contains digit");
         }
     }
 
     public void save(Weather weather) {
         weatherRepository.save(weather);
-    }
-
-    public void deleteWeatherById(long id) {
-        weatherRepository
-                .findById(id)
-                .ifPresent(weatherRepository::delete);
     }
 }
