3b8cfd877c035a19ee2d9604eefa6baba6ed759f
alim <asas@gmail.com>
2024-01-20 07:53:09 +0400

    removed search for one Weather because of duplicating code

--- src/main/java/com/laborotoryproject/weather/parser/repository/WeatherRepository.java
+++ src/main/java/com/laborotoryproject/weather/parser/repository/WeatherRepository.java
@@ -4,7 +4,6 @@ import com.laborotoryproject.weather.parser.entity.Weather;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,9 +11,8 @@ import java.util.Optional;
 public interface WeatherRepository extends JpaRepository<Weather, Long> {
 
     Optional<Weather> findWeatherByCityName(String citiName);
-    Optional<List<Weather>> findWeatherByTemperature(String temperature);
-    Optional<List<Weather>> findWeatherByPressure(String pressure);
-    Optional<List<Weather>> findWeatherBySpeed(String speed);
-    Optional<List<Weather>> findWeatherByHumidity(String humidity);
-
+    Optional<List<Weather>> findAllWeathersByTemperature(String temperature);
+    Optional<List<Weather>> findAllWeathersByPressure(String pressure);
+    Optional<List<Weather>> findAllWeathersBySpeed(String speed);
+    Optional<List<Weather>> findAllWeathersByHumidity(String humidity);
 }
