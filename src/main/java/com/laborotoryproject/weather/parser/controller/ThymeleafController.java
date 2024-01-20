d1cafd596fc14ac479b54c4ff1de32492dfef8bf
alim <asas@gmail.com>
2024-01-20 07:56:16 +0400

    changed name of the methods

--- src/main/java/com/laborotoryproject/weather/parser/controller/ThymeleafController.java
+++ src/main/java/com/laborotoryproject/weather/parser/controller/ThymeleafController.java
@@ -47,56 +47,56 @@ public class ThymeleafController {
     @GetMapping("/getWeathersByTemp")
     public String getWeathersByTemp(@RequestParam String temperature, Model model) {
         model.addAttribute("weathersByTemp",
-                weatherService.findByTemperature(temperature).stream().map(weatherDtoFactory::makeDto));
+                weatherService.findAllWeathersByTemperature(temperature).stream().map(weatherDtoFactory::makeDto));
         return "weathersByTemp";
     }
 
     @GetMapping("/getWeathersByPressure")
     public String getWeathersByPressure(@RequestParam String pressure, Model model) {
         model.addAttribute("weathersByPressure",
-                weatherService.findByPressure(pressure).stream().map(weatherDtoFactory::makeDto));
+                weatherService.findAllWeathersByPressure(pressure).stream().map(weatherDtoFactory::makeDto));
         return "weathersByPressure";
     }
 
     @GetMapping("/getWeathersBySpeed")
     public String getWeathersBySpeed(@RequestParam String speed, Model model) {
         model.addAttribute("weathersBySpeed",
-                weatherService.findBySpeed(speed).stream().map(weatherDtoFactory::makeDto));
+                weatherService.findAllWeathersBySpeed(speed).stream().map(weatherDtoFactory::makeDto));
         return "weathersBySpeed";
     }
 
     @GetMapping("/getWeathersByHumidity")
     public String getWeathersByHumidity(@RequestParam String humidity, Model model) {
         model.addAttribute("weathersByHumidity",
-                weatherService.findByHumidity(humidity).stream().map(weatherDtoFactory::makeDto));
+                weatherService.findAllWeathersByHumidity(humidity).stream().map(weatherDtoFactory::makeDto));
         return "weathersByHumidity";
     }
 
     @GetMapping("/getWeatherByTemperature")
     public String getWeatherByTemperature(@RequestParam String temperature, Model model) {
         model.addAttribute("weatherByTemperature",
-                weatherDtoFactory.makeDto(weatherService.findByTemperature(temperature).stream().findAny().get()));
+                weatherDtoFactory.makeDto(weatherService.findAllWeathersByTemperature(temperature).stream().findFirst().get()));
         return "weatherByTemperature";
     }
 
     @GetMapping("/getWeatherByPressure")
     public String getWeatherByPressure(@RequestParam String pressure, Model model) {
         model.addAttribute("weatherByPressure",
-                weatherDtoFactory.makeDto(weatherService.findByPressure(pressure).stream().findAny().get()));
+                weatherDtoFactory.makeDto(weatherService.findAllWeathersByPressure(pressure).stream().findFirst().get()));
         return "weatherByPressure";
     }
 
     @GetMapping("/getWeatherBySpeed")
     public String getWeatherBySpeed(@RequestParam String speed, Model model) {
         model.addAttribute("weatherBySpeed",
-                weatherDtoFactory.makeDto(weatherService.findBySpeed(speed).stream().findAny().get()));
+                weatherDtoFactory.makeDto(weatherService.findAllWeathersBySpeed(speed).stream().findFirst().get()));
         return "weatherBySpeed";
     }
 
     @GetMapping("/getWeatherByHumidity")
     public String getWeatherByHumidity(@RequestParam String humidity, Model model) {
         model.addAttribute("weatherByHumidity",
-                weatherDtoFactory.makeDto(weatherService.findByHumidity(humidity).stream().findAny().get()));
+                weatherDtoFactory.makeDto(weatherService.findAllWeathersByHumidity(humidity).stream().findFirst().get()));
         return "weatherByHumidity";
     }
 }
