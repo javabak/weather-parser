63f9f6feb626a71f9b32e51cac5a198cedf02cf6
alim <asas@gmail.com>
2024-01-20 07:55:08 +0400

    added makeListDto method

--- src/main/java/com/laborotoryproject/weather/parser/factory/WeatherDtoFactory.java
+++ src/main/java/com/laborotoryproject/weather/parser/factory/WeatherDtoFactory.java
@@ -1,8 +1,11 @@
 package com.laborotoryproject.weather.parser.factory;
 
-import com.laborotoryproject.weather.parser.entity.Weather;
 import com.laborotoryproject.weather.parser.dto.WeatherDto;
+import com.laborotoryproject.weather.parser.entity.Weather;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class WeatherDtoFactory {
@@ -16,5 +19,9 @@ public class WeatherDtoFactory {
                 .speed(weather.getSpeed())
                 .temperature(weather.getTemperature())
                 .build();
+    }
+
+    public List<WeatherDto> makeListDto(List<Weather> weathers) {
+        return weathers.stream().map(this::makeDto).collect(Collectors.toList());
     }
 }
