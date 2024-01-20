5a72099d2c988d80be52e984a2c7ac53921ecd45
alim <asas@gmail.com>
2024-01-20 07:55:33 +0400

    added custom exception for all exceptions where whether not found

--- src/main/java/com/laborotoryproject/weather/parser/exception/request_exceptions/WeatherNotFound.java
+++ src/main/java/com/laborotoryproject/weather/parser/exception/request_exceptions/WeatherNotFound.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class WeatherWithIdNotFoundException extends RuntimeException {
-    public WeatherWithIdNotFoundException(String message) {
+public class WeatherNotFound extends RuntimeException {
+    public WeatherNotFound(String message) {
         super(message);
     }
 }
