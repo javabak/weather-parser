c35fb262416cb7dca5428c328574a49873602e19
alim <asas@gmail.com>
2024-01-22 03:19:03 +0400

    wrote tests for validating of pressure,speed,cityName

--- src/main/test/com/laborotoryproject/weatherparser/utils/validate/ValidatingDataTest.java
+++ src/main/test/com/laborotoryproject/weatherparser/utils/validate/ValidatingDataTest.java
@@ -1,58 +1,74 @@
 package com.laborotoryproject.weatherparser.utils.validate;
 
-import com.laborotoryproject.weather.parser.util.validate.ValidatingData;
 import org.junit.jupiter.api.Test;
 
+import static com.laborotoryproject.weather.parser.util.validate.ValidatingData.*;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ValidatingDataTest {
 
-    ValidatingData validatingData = new ValidatingData();
 
     @Test
-    public void testCheckTemperatureInput() {
+    public void testValidateTemperature() {
 
-        // Valid Inputs
-        assertTrue(validatingData.checkTemperatureInput("25"));
-        assertTrue(validatingData.checkTemperatureInput("-10"));
-        assertTrue(validatingData.checkTemperatureInput("0"));
-        assertTrue(validatingData.checkTemperatureInput("12.5"));
-        assertTrue(validatingData.checkTemperatureInput("-3.75"));
+        assertTrue(validateTemperature("25"));
+        assertTrue(validateTemperature("-10"));
+        assertTrue(validateTemperature("0"));
+        assertTrue(validateTemperature("12.5"));
+        assertTrue(validateTemperature("-3.75"));
 
-        // Invalid inputs
-        assertFalse(validatingData.checkTemperatureInput("abc"));
-        assertFalse(validatingData.checkTemperatureInput("1a"));
-        assertFalse(validatingData.checkTemperatureInput(""));
-        assertFalse(validatingData.checkTemperatureInput("0.0.0"));
-        assertFalse(validatingData.checkTemperatureInput("-"));
+        assertFalse(validateTemperature("abc"));
+        assertFalse(validateTemperature("1a"));
+        assertFalse(validateTemperature(""));
+        assertFalse(validateTemperature("0.0.0"));
+        assertFalse(validateTemperature("-"));
     }
 
     @Test
-    public void testCheckStringNotContainsDigit() {
-        assertTrue(validatingData.checkStringNotContainsDigit("abc"));
+    public void testValidatePressure() {
+        assertTrue(validatePressure("12 мм"));
+        assertTrue(validatePressure("1 мм"));
 
-        assertFalse(validatingData.checkStringNotContainsDigit("23asd"));
+
+        assertFalse(validatePressure("мм"));
+        assertFalse(validatePressure("12мм"));
+        assertFalse(validatePressure("мм21"));
+        assertFalse(validatePressure("мм "));
+        assertFalse(validatePressure("12 ab"));
     }
 
     @Test
-    public void testCheckStringStartWithDigitAndContainsLetter() {
-        assertTrue(validatingData.checkStringStartWithDigitAndContainsLetter("123 dad"));
+    public void testValidateHumidity() {
+        assertTrue(validateHumidity("12"));
+        assertTrue(validateHumidity("6"));
 
-        assertFalse(validatingData.checkStringStartWithDigitAndContainsLetter("123"));
+        assertFalse(validateHumidity("ab"));
+        assertFalse(validateHumidity("adc12"));
+        assertFalse(validateHumidity("21adc"));
+        assertFalse(validateHumidity("adc "));
+        assertFalse(validateHumidity("12 adc"));
     }
 
     @Test
-    public void testCheckStringForHumidity() {
-        assertTrue(validatingData.checkStringForHumidity("53%"));
+    public void testValidateSpeed() {
+        assertTrue(validateSpeed("12 мс"));
+        assertTrue(validateSpeed("1 мс"));
 
-        assertFalse(validatingData.checkStringForHumidity("21"));
+        assertFalse(validateSpeed("12"));
+        assertFalse(validateSpeed("12мс"));
+        assertFalse(validateSpeed("мс12"));
+        assertFalse(validateSpeed("12 "));
+        assertFalse(validateSpeed("12 ab"));
     }
 
     @Test
-    public void testCheckId() {
-        assertTrue(validatingData.checkId("13"));
+    public void testValidateCityName() {
+        assertTrue(validateCityName("moscow"));
 
-        assertFalse(validatingData.checkId("asd123"));
+        assertFalse(validateCityName("123"));
+        assertFalse(validateCityName("ab12"));
+        assertFalse(validateCityName("21ab"));
+        assertFalse(validateCityName("ab 12"));
     }
 }
